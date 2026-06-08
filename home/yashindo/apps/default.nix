02cb993f0415c084d295b13d4505ec45a0a5e480
Operator-Syn <johnronanbeira100404@gmail.com>
2026-06-08 10:29:55 +0800

    add: default.nix

---
+++ home/yashindo/apps/default.nix
@@ -0,0 +1,39 @@
+{
+  imports = [
+    ./alacritty.nix
+    ./alacritty-widget.nix
+    ./brave.nix
+    ./cava.nix
+    ./direnv.nix
+    ./discord.nix
+    ./discord-pro.nix
+    ./fastfetch.nix
+    ./firefox.nix
+    ./fish.nix
+    ./latex.nix
+    ./spotify-player.nix
+    ./starship.nix
+    ./vscode.nix
+    ./winboat.nix
+    ./youtube-dl.nix
+  ];
+
+  modules = {
+    alacritty.enable = true;
+    alacritty-widget.enable = false;
+    brave.enable = true;
+    cava.enable = true;
+    direnv.enable = true;
+    discord.enable = true;
+    discord-pro.enable = true;
+    fastfetch.enable = true;
+    firefox.enable = true;
+    fish.enable = true;
+    latex.enable = true;
+    spotify-player.enable = false;
+    starship.enable = true;
+    vscode.enable = true;
+    winboat.enable = true;
+    youtube-dl.enable = true;
+  };
+}
