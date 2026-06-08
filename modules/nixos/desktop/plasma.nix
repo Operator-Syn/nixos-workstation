24e82f0679f331033654b934bbeda71486daa7d0
Operator-Syn <johnronanbeira100404@gmail.com>
2026-06-08 10:29:56 +0800

    add: plasma.nix

---
+++ modules/nixos/desktop/plasma.nix
@@ -0,0 +1,27 @@
+{
+  programs.firefox.enable = true;
+
+  security.rtkit.enable = true;
+
+  services = {
+    desktopManager.plasma6.enable = true;
+    displayManager.sddm.enable = true;
+    printing.enable = true;
+    pulseaudio.enable = false;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+  };
+}
