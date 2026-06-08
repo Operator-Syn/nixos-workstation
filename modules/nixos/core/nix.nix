15c67c9a24f5953e396a3f7600cf8f001cecf6b5
Operator-Syn <johnronanbeira100404@gmail.com>
2026-06-08 10:29:56 +0800

    add: nix.nix

---
+++ modules/nixos/core/nix.nix
@@ -0,0 +1,19 @@
+{
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+}
