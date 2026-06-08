95058627dc1acd45cebe1e6a0107bbbee7f0c2a9
Operator-Syn <johnronanbeira100404@gmail.com>
2026-06-08 10:24:36 +0800

    home: trim global runtimes

---
+++ home/yashindo/packages.nix
@@ -0,0 +1,26 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    bat
+    btop
+    cudaPackages.cudatoolkit
+    exfat
+    fzf
+    gparted
+    inkscape
+    inter
+    inter-nerdfont
+    kdePackages.kdegraphics-thumbnailers
+    kdePackages.kirigami
+    kdePackages.kirigami-addons
+    kdePackages.qwt
+    lsd
+    miracode
+    nerd-fonts.fira-code
+    nerd-fonts.fira-mono
+    openmpi
+    # pipenv
+    vlc
+    zoxide
+    zoom-us
+  ];
+}
