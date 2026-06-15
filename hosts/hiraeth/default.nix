5d43c375dc7a10eefa07785e4a40014443b4c853
Operator-Syn <johnronanbeira100404@gmail.com>
2026-06-15 11:34:03 +0800

    Enable Hiraeth networking, gaming, and Python shell modules.

--- hosts/hiraeth/default.nix
+++ hosts/hiraeth/default.nix
@@ -6,25 +6,34 @@
 
     ../../modules/nixos/core
     ../../modules/nixos/desktop/plasma.nix
+
     ../../modules/nixos/development/nix-ld.nix
     ../../modules/nixos/development/python-shell.nix
     ../../modules/nixos/development/distrobox.nix
     ../../modules/nixos/development/distrobox-debian-dev.nix
     # ../../modules/nixos/development/debian-container.nix
+
     ../../modules/nixos/hardware/bluetooth.nix
     ../../modules/nixos/hardware/msi.nix
+
     ../../modules/nixos/networking.nix
+    ../../modules/nixos/netbird.nix
     ../../modules/nixos/packages.nix
     ../../modules/nixos/users/yashindo.nix
     ../../modules/nixos/virtualisation.nix
+    ../../modules/nixos/steam.nix
   ];
 
   networking.hostName = "Hiraeth";
 
   modules = {
+    netbird.enable = true;
+    steam.enable = true;
+
     distrobox.debian-dev.enable = true;
     # debian-container.enable = true;
-    # python-shell.enable = true;
+
+    python-shell.enable = true;
   };
 
   system.stateVersion = "25.11";
