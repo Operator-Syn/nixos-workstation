b713a5d84ddf2d439d6051191f86577811b25093
Operator-Syn <johnronanbeira100404@gmail.com>
2026-06-25 08:07:20 +0800

    Remove the Discover update setting.

--- home/yashindo/plasma/desktop-settings.nix
+++ home/yashindo/plasma/desktop-settings.nix
@@ -1,7 +1,10 @@
-{lib, ...}: {
-  programs.plasma.configFile = {
-    "discoverrc"."Software" = {
-      UseOfflineUpdates = true;
+{pkgs, ...}: {
+  programs.plasma = {
+    hotkeys.commands.open-alacritty = {
+      name = "Open Alacritty";
+      comment = "Open Alacritty";
+      key = "Ctrl+Shift+T";
+      command = "${pkgs.alacritty-graphics}/bin/alacritty";
     };
   };
 }
