437224e4227c9c654d73a766ecb6f84a38942b6f
Operator-Syn <johnronanbeira100404@gmail.com>
2026-07-16 08:04:30 +0800

    Make MSI hardware support optional.

--- modules/nixos/hardware/msi.nix
+++ modules/nixos/hardware/msi.nix
@@ -1,8 +1,10 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: let
+  cfg = config.modules.msi;
   msiBootScript = pkgs.writeShellScript "msi-hardware-boot" ''
     if [ -d /sys/devices/platform/msi-ec ]; then
       echo turbo > /sys/devices/platform/msi-ec/shift_mode
@@ -12,40 +14,44 @@
     fi
   '';
 in {
-  boot.extraModulePackages = [config.boot.kernelPackages.msi-ec];
-  boot.kernelModules = [
-    "msi-ec"
-    "ec_sys"
-  ];
+  options.modules.msi.enable = lib.mkEnableOption "MSI hardware controls";
 
-  boot.kernelParams = [
-    "ec_sys.write_support=1"
-  ];
+  config = lib.mkIf cfg.enable {
+    boot.extraModulePackages = [config.boot.kernelPackages.msi-ec];
+    boot.kernelModules = [
+      "msi-ec"
+      "ec_sys"
+    ];
 
-  systemd.services.msi-hardware-boot = {
-    description = "Set MSI EC shift_mode to turbo and fan_mode to advanced";
-    wantedBy = ["multi-user.target"];
-    after = ["systemd-modules-load.service"];
+    boot.kernelParams = [
+      "ec_sys.write_support=1"
+    ];
 
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = "${msiBootScript}";
+    systemd.services.msi-hardware-boot = {
+      description = "Set MSI EC shift_mode to turbo and fan_mode to advanced";
+      wantedBy = ["multi-user.target"];
+      after = ["systemd-modules-load.service"];
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "${msiBootScript}";
+      };
     };
+
+    environment.etc."xdg/autostart/mcontrolcenter.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Exec=mcontrolcenter
+      Hidden=false
+      NoDisplay=false
+      X-GNOME-Autostart-enabled=true
+      Name=mControlCenter
+      Comment=MSI Center for Linux
+    '';
+
+    environment.systemPackages = [
+      pkgs.mcontrolcenter
+    ];
   };
-
-  environment.etc."xdg/autostart/mcontrolcenter.desktop".text = ''
-    [Desktop Entry]
-    Type=Application
-    Exec=mcontrolcenter
-    Hidden=false
-    NoDisplay=false
-    X-GNOME-Autostart-enabled=true
-    Name=mControlCenter
-    Comment=MSI Center for Linux
-  '';
-
-  environment.systemPackages = [
-    pkgs.mcontrolcenter
-  ];
 }
