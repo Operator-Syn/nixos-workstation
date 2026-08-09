dd1f6154ddd8b783dff412782f71201f01550624
Operator-Syn <johnronanbeira100404@gmail.com>
2026-08-09 22:00:17 +0800

    Checkpoint: hosts/hiraeth/default.nix
    Co-authored-by: Feilhann <feilhann@evergarde.syn-forge.com>

--- hosts/hiraeth/default.nix
+++ hosts/hiraeth/default.nix
@@ -1,8 +1,6 @@
 {inputs, ...}: {
   imports = [
     inputs.aagl.nixosModules.default
-    inputs.sops-nix.nixosModules.sops
-
     ./hardware-configuration.nix
     ./storage.nix
     ./boot.nix
@@ -27,42 +25,11 @@
     ../../modules/nixos/openssh.nix
     ../../modules/nixos/netbird.nix
     ../../modules/nixos/packages.nix
-    ../../modules/nixos/hermes.nix
-    ../../modules/nixos/hermes-ssh.nix
-    ../../modules/nixos/obsidian-hermes-vault.nix
-    ../../modules/nixos/graphify.nix
-    ../../modules/nixos/hermes-graphify.nix
     ../../modules/nixos/users/yashindo.nix
     ../../modules/nixos/kvm-manager.nix
     ../../modules/nixos/virtualisation.nix
     ../../modules/nixos/steam.nix
   ];
-
-  sops.age.keyFile = "/home/yashindo/.config/sops/age/keys.txt";
-  sops.secrets.gh_token = {
-    sopsFile = ../../secrets/gh.yaml;
-    key = "token";
-    path = "/run/hermes/gh/feilhann.token";
-    owner = "yashindo";
-    group = "users";
-    mode = "0400";
-  };
-  sops.secrets.gh_operator_syn_token = {
-    sopsFile = ../../secrets/gh.yaml;
-    key = "operator_syn_token";
-    path = "/run/hermes/gh/operator-syn.token";
-    owner = "yashindo";
-    group = "users";
-    mode = "0400";
-  };
-  sops.secrets.hermes_ssh_id_ed25519 = {
-    sopsFile = ../../secrets/ssh.yaml;
-    key = "data";
-    path = "/run/hermes/ssh/id_ed25519";
-    owner = "yashindo";
-    group = "users";
-    mode = "0400";
-  };
 
   nix.settings = inputs.aagl.nixConfig;
 
@@ -79,10 +46,6 @@
 
     python-shell.enable = true;
     asus.enable = true;
-    hermes.enable = true;
-    obsidianHermesVault.enable = true;
-    graphify.enable = true;
-    hermes.graphify.enable = true;
   };
 
   hostStorage.enable = true;
