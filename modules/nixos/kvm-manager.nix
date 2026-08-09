402c17a05c93a8814b441aff3804bf039afa5e23
Operator-Syn <johnronanbeira100404@gmail.com>
2026-08-09 22:00:18 +0800

    Checkpoint: modules/nixos/kvm-manager.nix
    Co-authored-by: Feilhann <feilhann@evergarde.syn-forge.com>

--- modules/nixos/kvm-manager.nix
+++ modules/nixos/kvm-manager.nix
@@ -20,5 +20,22 @@ in {
         swtpm.enable = true;
       };
     };
+
+    systemd.services.libvirt-default-network = {
+      description = "Start the default libvirt network";
+      wantedBy = ["multi-user.target"];
+      wants = ["libvirtd.service"];
+      after = ["libvirtd.service"];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      script = ''
+        ${pkgs.libvirt}/bin/virsh -c qemu:///system net-autostart default
+        if ! ${pkgs.libvirt}/bin/virsh -c qemu:///system net-info default | ${pkgs.gnugrep}/bin/grep -q 'Active:.*yes'; then
+          ${pkgs.libvirt}/bin/virsh -c qemu:///system net-start default
+        fi
+      '';
+    };
   };
 }
