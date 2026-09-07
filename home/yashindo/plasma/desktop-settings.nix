1145d37ae051313fcb91a1e8676894ef68e78f22
test <test@example.invalid>
2026-09-07 16:07:15 +0800

    Disable KWin's blur effect.

--- home/yashindo/plasma/desktop-settings.nix
+++ home/yashindo/plasma/desktop-settings.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
   programs.plasma = {
+    # Disable KWin's blur effect while investigating offscreen framebuffer errors.
+    kwin.effects.blur.enable = false;
+
     hotkeys.commands.open-alacritty = {
       name = "Open Alacritty";
       comment = "Open Alacritty";
