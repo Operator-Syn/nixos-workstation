31ed2d9aed1bae9bd1313e5059fcd14bdb9d4b22
test <test@example.invalid>
2026-09-13 15:05:28 +0800

    Refresh Hiraeth initrd hardware modules.

--- hosts/hiraeth/hardware-configuration.nix
+++ hosts/hiraeth/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
