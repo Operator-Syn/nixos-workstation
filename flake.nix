0b8db4de82953011ba39c9adedc59ea966d00729
test <test@example.invalid>
2026-09-16 00:04:31 +0800

    Pin the BedrockOnLinux input to the v2.2.5 tag.

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     bedrock-on-linux = {
-      url = "github:Wyze3306/BedrockOnLinux/v2.2.4";
+      url = "github:Wyze3306/BedrockOnLinux/v2.2.5";
       flake = false;
     };
 
